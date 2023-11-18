dbd62c5e26c01b3686854c9d83ce744097a33cd9
infectedsoul01 <afzalbabu7860@gmail.com>
2023-11-18 11:02:58 +0530

    Subtarction

---
+++ calc.c
@@ -0,0 +1,23 @@
+void add()
+{
+    int a,b;
+    printf("Enter two number ");
+    scanf("%d%d",&a,&b);
+    printf("the sum of given number is %d",a+b);
+    
+}
+void subtraction()
+{
+    int a,b;
+    printf("enter two number ");
+    scanf("%d%d",&a,&b);
+    printf("Subtraction is %d",a-b);
+}
+int main()
+{
+    char ch;
+    printf("what do you want to perform +,-,* or /");
+    if(ch == '+')
+        add();
+
+}
